d3638b6d40b70881628d9fbe3c48cad59e887f6e
talonbrinkman <61207144+talonbrinkman@users.noreply.github.com>
2022-08-28 19:35:23 -0500

    Add files via upload

--- updateChannels.php
+++ updateChannels.php
@@ -3,6 +3,8 @@
   $username = "root";
   $password = "";
   $dbname = "simplechatroom";
+
+  $search = $_POST['postsearch'];
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
@@ -14,8 +16,8 @@
   $result = mysqli_query($conn, $sql);
   if(mysqli_num_rows($result) > 0){
     while ($row = mysqli_fetch_assoc($result)){
-      if($row['channelPassword'] == ''){
-        echo '<div class="channel"><h1>' . $row['channelName'] . '<span>' . $row['channelHost'] . '</span></h1><input type="button" value="Join"></div>';
+      if($row['channelPassword'] == '' && (str_contains($row['channelHost'],$search) || str_contains($row['channelName'],$search))){
+        echo '<div class="channel"><h1>' . $row['channelName'] . '<span>' . $row['channelHost'] . '</span></h1><button id="join" onclick="joinChannel()" disabled data-value="' . $row['channelName'] . '">Join</div>';
       }
     }
   }
