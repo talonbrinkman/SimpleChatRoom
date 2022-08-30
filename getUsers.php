dca17035bd65b6d3205028a8930331cf9bf5742f
talonbrinkman <61207144+talonbrinkman@users.noreply.github.com>
2022-08-29 19:13:02 -0500

    Add files via upload

---
+++ getUsers.php
@@ -0,0 +1,32 @@
+<?php
+  $search = $_POST['postsearch'];
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "simplechatroom";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  //Gets Users
+  $sql = "SELECT * FROM users";
+  $result = mysqli_query($conn, $sql);
+  if(mysqli_num_rows($result) > 0){
+    while ($row = mysqli_fetch_assoc($result)){
+      $username = $row['username'];
+      $channel = $row['channel'];
+      if(str_contains($username,$search) || str_contains($channel,$search)){
+        echo '<div class="user"><h1>' . $username . '<span> ' . $channel . '</span></h1></div>';
+      }
+    }
+  }
+  else{
+    echo '<div class="alert">Currently No Users</div>';
+  }
+  //Disconnects
+  $conn->close();
+?>
